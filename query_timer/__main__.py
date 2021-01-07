0262d07050ff60ddab5207b1fb97705d9c455f0e
Joshua Hercher <josh@crate.io>
2021-01-07 15:17:24 +0100

    Replace curses with blessed for query_timer output

--- query_timer/__main__.py
+++ query_timer/__main__.py
@@ -1,10 +1,10 @@
-import curses
 import logging
 import numpy
 import urllib3
 import statistics
 import time
 import shutil
+from blessed import Terminal
 from queue import Queue
 from tictrack import tic_toc, timed_function
 from threading import Thread
@@ -19,7 +19,7 @@ from data_generator.db_writer import DbWriter
 from query_timer.config import QueryTimerConfig
 from query_timer.argument_parser import parse_arguments
 
-console = curses.initscr()
+terminal = Terminal()
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
@@ -58,6 +58,13 @@ def get_db_writer() -> DbWriter:  # noqa
     return db_writer
 
 
+def percentage_to_rgb(percentage):
+    red = (percentage - 50) / 100.0 if percentage > 50 else 1.0
+    green = 1.0 if percentage > 50 else 2 * percentage / 100.0
+    blue = 0.0
+    return red * 255, green * 255, blue * 255
+
+
 @timed_function()
 def print_progressbar(iteration, total, prefix="", suffix="", decimals=1, length=100, fill='█'):
     """
@@ -71,23 +78,26 @@ def print_progressbar(iteration, total, prefix="", suffix="", decimals=1, length
         length      - Optional  : character length of bar (Int)
         fill        - Optional  : bar fill character (Str)
     """
-    now = time.time()
+    duration = time.time() - start_time
     percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    r, g, b = percentage_to_rgb(float(percent))
     filled_length = int(length * iteration // total)
     bar = fill * filled_length + '-' * (length - filled_length)
 
     if "execute_query" in tic_toc:
         values = tic_toc["execute_query"]
-        console.addstr(3, 0, f"{prefix} |{bar}| {percent}%% {suffix} {round(now - start_time, 2)}s")
+        print(terminal.move_y(3) + f"{prefix} |{terminal.color_rgb(r, g, b)}{bar}{terminal.normal}| {percent}% "
+                                   f"{suffix} {round(duration, 2)}s")
+        print(f"time left: {round(((duration / float(percent)) * 100) - duration, 2)}s                                ")
         if len(values) > 1:
             pass
-            console.addstr(5, 0, f"rate : {round((1 / numpy.mean(values)) * config.concurrency, 3)}qs/s\n")
-            console.addstr(6, 0, f"mean : {round(numpy.mean(values) * 1000, 3)}ms\n")
-            console.addstr(7, 0, f"stdev: {round(numpy.std(values) * 1000, 3)}ms\n")
-            console.addstr(8, 0, f"min  : {round(min(values) * 1000, 3)}ms\n")
-            console.addstr(9, 0, f"max  : {round(max(values) * 1000, 3)}ms\n")
-            console.addstr(10, 0, f"success: {success} - failure: {failure}")
-    console.refresh()
+            print(terminal.move_y(6) + f"rate   : {round((1 / numpy.mean(values)) * config.concurrency, 3)}qs/s       ")
+            print(f"mean   : {round(numpy.mean(values) * 1000, 3)}ms       ")
+            print(f"stdev  : {round(numpy.std(values) * 1000, 3)}ms      ")
+            print(f"min    : {round(min(values) * 1000, 3)}ms       ")
+            print(f"max    : {round(max(values) * 1000, 3)}ms       ")
+            print(f"success: {terminal.green}{success}{terminal.normal}      ")
+            print(f"failure: {terminal.red}{failure}{terminal.normal}        ")
 
 
 def start_query_run():
@@ -129,31 +139,28 @@ def run_qt():
 
 def main():
     global config
+    with terminal.hidden_cursor():
+        # load configuration an set everything up
+        config = parse_arguments(config)
+        valid_config = config.validate_config()
+        if not valid_config:
+            logging.error(f"invalid configuration: {config.invalid_configs}")
+            exit(-1)
 
-    # load configuration an set everything up
-    config = parse_arguments(config)
-    valid_config = config.validate_config()
-    if not valid_config:
-        logging.error(f"invalid configuration: {config.invalid_configs}")
-        exit(-1)
+        print(f"concurrency: {config.concurrency}\niterations : {config.iterations}")
+        terminal_size = shutil.get_terminal_size()
+        print_progressbar(0, config.concurrency * config.iterations,
+                          prefix='Progress:', suffix='Complete', length=(terminal_size.columns - 40))
 
-    console.addstr(f"""concurrency: {config.concurrency}\niterations : {config.iterations}""")
-    console.refresh()
-    terminal_size = shutil.get_terminal_size()
-    print_progressbar(0, config.concurrency * config.iterations,
-                      prefix='Progress:', suffix='Complete', length=(terminal_size.columns - 40))
+        run_qt()
 
-    run_qt()
-
-    if "execute_query" in tic_toc:
-        values = tic_toc["execute_query"]
-        qus = statistics.quantiles(values, n=100, method="inclusive")
-        line = 11
-        for i in range(0, len(qus)):
-            if str(i + 1) in config.quantiles:
-                line += 1
-                console.addstr(line, 0, f"p{i+1}  : {round(qus[i]*1000, 3)}ms")
-    console.refresh()
+        if "execute_query" in tic_toc:
+            values = tic_toc["execute_query"]
+            qus = statistics.quantiles(values, n=100, method="inclusive")
+            print("")
+            for i in range(0, len(qus)):
+                if str(i + 1) in config.quantiles:
+                    print(f"p{i+1}  : {round(qus[i]*1000, 3)}ms")
 
 
 if __name__ == '__main__':
